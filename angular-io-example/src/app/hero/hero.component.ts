bdb344fad24e5af8570728f959dbb564e6fc527c
Eduardo Flores <efloresmiranda@dimaticasoftware.com>
2020-07-04 09:51:01 +0200

    I've added 'strictNullChecks' =tru, and 'noImplicitAny' =true in to tsconfig.json

--- angular-io-example/src/app/hero/hero.component.ts
+++ angular-io-example/src/app/hero/hero.component.ts
@@ -10,7 +10,7 @@ export class HeroComponent {
   @Input() hero: Hero;
   @Output() delete = new EventEmitter();
 
-  onDeleteClick($event): void {
+  onDeleteClick($event: any): void {
     $event.stopPropagation();
     this.delete.next();
   }
